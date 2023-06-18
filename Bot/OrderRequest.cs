55dd780652dbd53fcc3394ead8ba4d0c27491b7b
Hedge <35341367+HedgeCr33p@users.noreply.github.com>
2023-06-18 00:27:53 -0700

    Embed for user messages
    Embed for user message dms

--- Bot/OrderRequest.cs
+++ Bot/OrderRequest.cs
@@ -32,33 +32,52 @@ namespace SysBot.ACNHOrders
             VillagerOrder = vil;
         }
 
+        private async void SendMessageWithEmbed(SocketUser user, string title, string description, string url)
+        {
+            var embed = new EmbedBuilder()
+            {
+                Color = Color.Green,
+                Title = title,
+                Description = description,
+                ThumbnailUrl = url
+            };
+
+            await user.SendMessageAsync(embed: embed.Build());
+        }
+
         public void OrderCancelled(CrossBot routine, string msg, bool faulted)
         {
             OnFinish?.Invoke(routine);
-            Trader.SendMessageAsync($"Oops! Something has happened with your order: {msg}");
+            // Replace 'https://cancel.url' with the URL of the image you want to display when the order is cancelled
+            SendMessageWithEmbed(Trader, "Order Cancelled", $"Oops! Something has happened with your order: {msg}", "https://media0.giphy.com/media/J63ixMPVSJJzW/giphy.gif?cid=ecf05e477363n6jj6pqcbs8x87o302xugjy344l7j1gyiypw&ep=v1_gifs_search&rid=giphy.gif&ct=g");
             if (!faulted)
-                CommandSentChannel.SendMessageAsync($"{Trader.Mention} - Your order has been cancelled: {msg}");
+                // Replace 'https://cancel.url' with the URL of the image you want to display when the order is cancelled
+                SendMessageWithEmbed(Trader, "Order Cancelled", $"{Trader.Mention} - Your order has been cancelled: {msg}", "https://media0.giphy.com/media/J63ixMPVSJJzW/giphy.gif?cid=ecf05e477363n6jj6pqcbs8x87o302xugjy344l7j1gyiypw&ep=v1_gifs_search&rid=giphy.gif&ct=g");
         }
 
         public void OrderInitializing(CrossBot routine, string msg)
         {
-            Trader.SendMessageAsync($"Your order is starting, please **ensure your inventory is __empty__**, then go talk to Orville and stay on the Dodo code entry screen. I will send you the Dodo code shortly. {msg}");
+            // Replace 'https://init.url' with the URL of the image you want to display when the order is initializing
+            SendMessageWithEmbed(Trader, "Order Initializing", $"Your order is starting, please **ensure your inventory is __empty__**, then go talk to Orville and stay on the Dodo code entry screen. I will send you the Dodo code shortly. {msg}", "https://media1.giphy.com/media/WIwvGzMSd8jGU/giphy.gif?cid=ecf05e47k1y4kugcu7pkucoa27kx4ae7sw85igsmk66ym44y&ep=v1_gifs_related&rid=giphy.gif&ct=g");
         }
 
         public void OrderReady(CrossBot routine, string msg, string dodo)
         {
-            Trader.SendMessageAsync($"I'm waiting for you {(Trader is SocketGuildUser guildUser ? guildUser.Nickname : Trader.Username)}! {msg}. Your Dodo code is **{dodo}**");
+            // Replace 'https://ready.url' with the URL of the image you want to display when the order is ready
+            SendMessageWithEmbed(Trader, "Order Ready", $"I'm waiting for you {(Trader is SocketGuildUser guildUser ? guildUser.Nickname : Trader.Username)}! {msg}. Your Dodo code is **{dodo}**", "https://media4.giphy.com/media/gwVQ7vG6KODYd8u0aK/giphy.gif?cid=ecf05e47qhgyguqq74428jh9fr8bvj7ig4phjkyeabsf1ncw&ep=v1_gifs_search&rid=giphy.gif&ct=g");
         }
 
         public void OrderFinished(CrossBot routine, string msg)
         {
             OnFinish?.Invoke(routine);
-            Trader.SendMessageAsync($"Your order is complete, Thanks for your order! {msg}");
+            // Replace 'https://finish.url' with the URL of the image you want to display when the order is finished
+            SendMessageWithEmbed(Trader, "Order Finished", $"Your order is complete, Thanks for your order! {msg}", "https://media0.giphy.com/media/zZsqxlIovBl9S/giphy.gif?cid=ecf05e47qhgyguqq74428jh9fr8bvj7ig4phjkyeabsf1ncw&ep=v1_gifs_search&rid=giphy.gif&ct=g");
         }
 
         public void SendNotification(CrossBot routine, string msg)
         {
             Trader.SendMessageAsync(msg);
         }
+
     }
 }

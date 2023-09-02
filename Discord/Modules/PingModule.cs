8876055d81c143b7a7e4a67a0c32af982bb691ee
Hedge <trenthancock123@gmail.com>
2023-09-02 13:06:24 -0700

    Update Ping

--- Discord/Modules/PingModule.cs
+++ Discord/Modules/PingModule.cs
@@ -13,8 +13,11 @@ namespace SysBot.ACNHOrders
         {
             "https://media3.giphy.com/media/eNmWr9p3AjNd0F7xWd/giphy.gif?cid=ecf05e47qz5n5vg83nak14var9ie1pfbinkki0lzuvca7xbs&ep=v1_gifs_related&rid=giphy.gif&ct=g",
             "https://media.tenor.com/iehE0de38mkAAAAC/animal-crossing-hello.gif",
-            "https://media.tenor.com/zlW3iIc21joAAAAM/animal-crossing-animal-crossing-movie.gif",
-            "https://media.tenor.com/27tSiStpM58AAAAC/isabelle-animal-crossing.gif"
+            "https://media.tenor.com/Yxp2lmU_JO0AAAAC/isabelle-animal.gif",
+            "https://media.tenor.com/27tSiStpM58AAAAC/isabelle-animal-crossing.gif",
+            "https://media.tenor.com/X63peV172DwAAAAM/squeakoid-animal-crossing.gif",
+            "https://media.tenor.com/aMTgGRQyqBUAAAAC/animal-crossing-tom-nook.gif",
+            "https://gifdb.com/images/high/animal-crossing-anime-celeste-coffee-20sjdglh60pef1hx.gif"
             // Add as many as you want...
         };
 

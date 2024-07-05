6c562fbe6e5f93cda73656f1d9bff855e5bce2db
Poke-Legend <154709143+Poke-Legend@users.noreply.github.com>
2024-07-05 09:56:02 -0400

    Updated Nuggets
    Tweaked and Recoded dodo to work with latest SixLabors.Fonts and NLog

--- Tests/SysBot.ACNHOrders.Tests/obj/Release/net8.0/SysBot.ACNHOrders.Tests.AssemblyInfo.cs
+++ Tests/SysBot.ACNHOrders.Tests/obj/Release/net8.0/SysBot.ACNHOrders.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SysBot.ACNHOrders.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7efb8a3562e61199982b8c74e18b0eba71a94a15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21852f05fa9e914d9410c0ef15afd05e961a6fe1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SysBot.ACNHOrders.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SysBot.ACNHOrders.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Util/Drawing/DodoDraw.cs
+++ Util/Drawing/DodoDraw.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.Fonts;
 using SixLabors.ImageSharp.Drawing.Processing;
 using SixLabors.ImageSharp.Processing;
@@ -11,38 +12,40 @@ namespace SysBot.ACNHOrders
     {
         private string ImagePathTemplate { get; set; } = "dodo.png";
         private string FontPath { get; set; } = "dodo.ttf";
-        private string ImagePathOutput => "current" + ImagePathTemplate;
+        private string ImagePathOutput => "current_" + ImagePathTemplate;
 
         private readonly FontCollection FontCollection = new();
         private readonly FontFamily DodoFontFamily;
         private readonly Font DodoFont;
-        private readonly Image BaseImage;
-
-        private readonly TextOptions options;
-        private readonly TextGraphicsOptions tOptions;
+        private readonly Image<Rgba32> BaseImage;
 
         public DodoDraw(float fontPercentage = 100)
         {
-            DodoFontFamily = FontCollection.Install(FontPath);
-            BaseImage = Image.Load(ImagePathTemplate);
-            DodoFont = DodoFontFamily.CreateFont(BaseImage.Height * 0.4f * (fontPercentage/100f), FontStyle.Regular);
-
-            options = new TextOptions()
-            {
-                ApplyKerning = true,
-                HorizontalAlignment = HorizontalAlignment.Center,
-                VerticalAlignment = VerticalAlignment.Center
-            };
-
-            tOptions = new TextGraphicsOptions()
-            {
-                TextOptions = options
-            };
+            DodoFontFamily = FontCollection.Add(FontPath);
+            BaseImage = Image.Load<Rgba32>(ImagePathTemplate);
+            DodoFont = DodoFontFamily.CreateFont(BaseImage.Height * 0.4f * (fontPercentage / 100f), FontStyle.Regular);
         }
 
         public string Draw(string dodo)
         {
-            using (var img = BaseImage.Clone(x => x.DrawText(tOptions, dodo, DodoFont, Color.White, new PointF(BaseImage.Width * 0.5f, BaseImage.Height * 0.38f))))
+            var textOptions = new TextOptions(DodoFont)
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center
+            };
+
+            var drawingOptions = new DrawingOptions
+            {
+                GraphicsOptions = new GraphicsOptions
+                {
+                    Antialias = true,
+                    AntialiasSubpixelDepth = 16
+                }
+            };
+
+            var textPosition = new PointF(BaseImage.Width * 0.5f, BaseImage.Height * 0.38f);
+
+            using (var img = BaseImage.Clone(ctx => ctx.DrawText(drawingOptions, dodo, DodoFont, Color.White, textPosition)))
             {
                 img.Save(ImagePathOutput);
             }

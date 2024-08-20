b31ac0fd946716bcc17bbc7a5f13fcf32e12b9ff
Poke-Legend <bdkohn91@icloud.com>
2024-08-19 21:42:23 -0400

    Update 2.8 - Clean / Optimized RequireSudoAttribute

--- obj/Debug/net8.0/SysBot.ACNHOrders.AssemblyInfo.cs
+++ obj/Debug/net8.0/SysBot.ACNHOrders.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SysBot.ACNHOrders")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f493d4f14d0a4c27d75005c4028756271aeaf507")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7243f71eb2802323a521e162091c7097403563d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SysBot.ACNHOrders")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SysBot.ACNHOrders")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
